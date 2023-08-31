8659240f29ec11524a7bac5eb2a42bbf0dc3fc1a
zevbo <zevminskyprimus@gmail.com>
2023-08-31 12:39:57 -0400

    those materials shit was frigging random

--- urdf_compose/urdf_obj.py
+++ urdf_compose/urdf_obj.py
@@ -9,39 +9,6 @@ from urdf_compose.xml_utils import elements_equal
 
 # URDFObj should not be specific to us as Tutor
 
-std_materials = ET.fromstring(
-    """
-
-<!-- Materials -->
-<robot>
-    <material name="Black">
-        <color rgba="0.0 0.0 0.0 1.0" />
-    </material>
-    <material name="Red">
-        <color rgba="0.85 0.19  0.21 1.0" />
-    </material>
-    <material name="Blue">
-        <color rgba="0.28 0.52 0.92 1.0" />
-    </material>
-    <material name="Green">
-        <color rgba="0.23 0.72 0.32 1.0" />
-    </material>
-    <material name="Yellow">
-        <color rgba="0.95 0.76 0.05 1.0" />
-    </material>
-    <material name="White">
-        <color rgba="1.0 1.0 1.0 1.0" />
-    </material>
-    <material name="Silver">
-        <color rgba="0.753 0.753 0.753 1.0" />
-    </material>
-    <material name="DarkGrey">
-        <color rgba="0.2 0.2 0.2 1.0" />
-    </material>
-</robot>
-"""
-)
-
 
 class CheckURDFFailure(Exception):
     pass
@@ -103,20 +70,6 @@ class URDFObj:
         dest.touch(exist_ok=True)
         self.tree.write(str(dest), xml_declaration=True, encoding="UTF-8")
 
-    def _add_colors(self) -> None:
-        materials_used = set()
-        materials_defined = set()
-        for material in self.getroot().iter("material"):
-            name = material.attrib["name"]
-            if len(material) == 0:
-                materials_used.add(name)
-            else:
-                materials_defined.add(name)
-
-        for mat in std_materials:
-            if mat.attrib["name"] in materials_used and mat.attrib["name"] not in materials_defined:
-                self.getroot().append(copy.deepcopy(mat))
-
     def __hash__(self) -> int:
         return hash(id(self))
 
@@ -150,20 +103,11 @@ class ExplicitURDFObj(URDFObj):
         tree = ET.ElementTree()
         tree.parse(str(self.path))
         super().__init__(tree)
-        self._add_colors()
 
         if check:
-            check_file = self.path.parent / f"{self.path.name}_check_urdfOBJ{uuid1()}.urdf"
-            self.write_xml(check_file)
-            check_urdf_result = check_urdf(check_file)
-
-            if check_file.exists() and check_urdf_result is None:
-                check_file.unlink()
-            else:
-                raise RuntimeError(
-                    f"Attempted to create URDFObj, but given invalid urdf file {path}. You can"
-                    f"check adjusted file at {check_file}. {check_urdf_result=}",
-                )
+            check_urdf_result = check_urdf(path)
+            if check_urdf_result is not None:
+                raise check_urdf_result
 
     def __repr__(self) -> str:
         return f"ExplicitURDFObj from {self.path.name}"
